0ded244df8639ee4fc18af6f1def51c99edb1d20
Hoang Hai <nghhai2712@gmail.com>
2019-08-21 13:49:47 +0700

    add file

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,5 +1,5 @@
 @extends('frontend.master')
-@section('title','Home')
+@section('title','Trang chủ')
 @section('content')
 <div class="slider-area an-1 hm-1">
     <div class="bend niceties preview-2">
@@ -22,7 +22,7 @@
             </div>  
         </div>
         <!-- direction 2 -->
-        <div id="slider-direction-2" class="slider-direction">
+        {{-- <div id="slider-direction-2" class="slider-direction">
             <div class="slider-progress"></div>
             <div class="slider-content t-lfl s-tb slider-2 lft-pr">
                 <div class="title-container s-tb-c">
@@ -32,7 +32,7 @@
                     <a class="btn-title" href="">View collection</a>
                 </div>
             </div>  
-        </div>
+        </div> --}}
     </div>
 </div>
 <div class="our-product-area">
